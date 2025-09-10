879398001bc0f5ded995d89ccbba099dbf7f2923
apexraccoon <129546703+apexraccoon@users.noreply.github.com>
2025-09-11 00:51:56 +0300

    Added Gui for sudoku

---
+++ Sudoku Solver Gui/MainWindow.xaml.cs
@@ -0,0 +1,131 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sudoku_Solver_Gui
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int[] Board = new int[82];
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        void Button_Clicked(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            if (button.Content.ToString() == " ")
+            {
+                button.Content = 0;
+            }
+            if (Int32.Parse(button.Content.ToString()) < 9)
+            {
+                
+                button.Content = Int32.Parse(button.Content.ToString()) + 1;
+            }
+            else
+            {
+                button.Content = " ";
+            }
+        }
+        
+        void Solve_Algo(object sender,RoutedEventArgs e)
+        {
+            for (int i = 1; i < 82; i++)
+            {
+                Button button = ((Button)this.FindName("Btn" + i));
+                //find button and put it inside button_found
+                //check if it is space put zero else put number value
+                if (button.Content.ToString() == " ")
+                {
+                    Board[i] = 0;
+                }
+                else
+                {
+                    //store value in board
+                    Board[i] = Int32.Parse(button.Content.ToString());
+                }
+            }
+            solver solve = new solver();
+            //solve board
+            solve.solve_board(Board,1,1);
+            for (int i = 1; i < 82; i++)
+            {
+                Button button = ((Button)this.FindName("Btn" + i));
+                //put new board value inside that button
+                button.Content = Board[i];
+            }
+        }
+        void Clear_Board(object sender, RoutedEventArgs e)
+        {
+            for (int i = 1; i < 82; i++)
+            {
+                Button button = ((Button)this.FindName("Btn" + i));
+                button.Content = " ";
+                Board[i] = 0;
+            }
+        }
+        void Settings(object sender, RoutedEventArgs e)
+        {
+            //Hide Board and buttons
+            for (int i = 1; i < 82; i++)
+            {
+                Button button = ((Button)this.FindName("Btn" + i));
+                button.Visibility = Visibility.Hidden;
+            }
+            Button Button_solve = ((Button)this.FindName("BtnSolve"));
+            Button Button_Clear = ((Button)this.FindName("BtnClear"));
+            Button Button_Back = ((Button)this.FindName("BtnBack"));
+            Button Button_ChangeBackground = ((Button)this.FindName("BtnBackgroundChange"));
+            TextBox ChangeBackground = ((TextBox)this.FindName("BackgroundImage"));
+            Button_solve.Visibility = Visibility.Hidden;
+            Button_Clear.Visibility = Visibility.Hidden;
+            ((Button)sender).Visibility = Visibility.Hidden;
+            //Show Back button and settings stuff
+            Button_Back.Visibility = Visibility.Visible;
+            ChangeBackground.Visibility = Visibility.Visible;
+            Button_ChangeBackground.Visibility = Visibility.Visible;
+        }
+        void Back(object sender, RoutedEventArgs e)
+        {
+            //show board and buttons
+            for (int i = 1; i < 82; i++)
+            {
+                Button button = ((Button)this.FindName("Btn" + i));
+                button.Visibility = Visibility.Visible;
+            }
+            Button Button_solve = ((Button)this.FindName("BtnSolve"));
+            Button Button_Clear = ((Button)this.FindName("BtnClear"));
+            Button Button_Setting = ((Button)this.FindName("BtnSettings"));
+            Button Button_ChangeBackground = ((Button)this.FindName("BtnBackgroundChange"));
+            TextBox ChangeBackground = ((TextBox)this.FindName("BackgroundImage"));
+            Button_solve.Visibility = Visibility.Visible;
+            Button_Clear.Visibility = Visibility.Visible;
+            Button_Setting.Visibility = Visibility.Visible;
+            //hide settings options
+            ((Button)sender).Visibility = Visibility.Hidden;
+            Button_ChangeBackground.Visibility = Visibility.Hidden;
+            ChangeBackground.Visibility = Visibility.Hidden;
+        }
+        void ChangeBackground(object sender,RoutedEventArgs e)
+        {
+            Canvas canvas = (Canvas)this.FindName("MainCanvas");
+            TextBox ChangeBackground = ((TextBox)this.FindName("BackgroundImage"));
+            ImageBrush background_image = (ImageBrush)canvas.FindName("BackgroundCanvas");
+            string filepath = ChangeBackground.Text;
+            filepath = filepath.Trim('"');
+            background_image.ImageSource = new BitmapImage(new Uri(filepath, UriKind.Absolute));
+            this.Background = background_image;
+        }
+    }
+}
---
+++ Sudoku Solver Gui/Sudoku Solver.cs
@@ -0,0 +1,219 @@
+﻿
+class solver
+{
+    public solver()
+    {
+
+    }
+    public int solve_board(int[] Board, int place, int Number)
+    {
+        int counter_x = 1;//Used To make sure whole row is checked
+        int check_position = place + 1;//used to choose row elements
+        int counter_y = 1;//Used To make sure whole row is checked
+        int Num_above_it = 0;//used to count how many numbers above it to see which box it belongs to
+        int max_place_box = place; //used to find range of place in that box
+
+        //base case
+        if (place == 82)
+        {
+            return 1;
+        }
+        else
+        {
+            //it is empty
+            if (Board[place] == 0)
+            {
+                Board[place] = Number;
+                //while it is wrong
+                while (check_row(counter_x, check_position, place, Board) || check_column(counter_y, check_position, place, Board) || check_box(check_position, max_place_box, place, Num_above_it, Board))
+                {
+                    Number++;
+                    if (Number >= 10)
+                    {
+                        Board[place] = 0;
+                        return 0;
+                    }
+                    Board[place] = Number;
+                }
+                //no error found
+                Board[place] = Number;
+                //The number after it is impossible should backtrack
+                if (solve_board(Board, place + 1, 1) == 0)
+                {
+                    Number++;
+                    Board[place] = 0;
+                    //incase number was 9 for one before
+                    if (Number == 10)
+                    {
+                        return 0;
+                    }
+                    else
+                    {
+                        return solve_board(Board, place, Number);
+                    }
+                }
+                //it worked should continue
+                else
+                {
+                    return 1;
+                }
+            }
+            //skip it and go to next one
+            else
+            {
+                return solve_board(Board, place + 1, 1);
+            }
+        }
+    }
+
+    private bool check_row(int counter_x, int check_position, int place, int[] Board)
+    {
+        //make it first element in row
+        if (place % 9 == 0)//last element in row
+        {
+            check_position = place - 8;
+        }
+        else
+        {
+            check_position = place + 1;
+        }
+        while (counter_x < 8)
+        {
+            //error found
+            if (Board[place] == Board[check_position])
+            {
+                return true;
+            }
+            if (check_position % 9 == 0)
+            {
+                check_position -= 8;
+            }
+            else
+            {
+                check_position++;
+            }
+            counter_x++;
+        }
+        return false;
+    }
+    private bool check_column(int counter_y, int check_position, int place, int[] Board)
+    {
+        check_position = place + 9;
+        if (place >= 73 && place <= 81)//check if last row was reached
+        {
+            while (check_position > 9)
+            {
+                check_position -= 9;
+            }
+        }
+        counter_y = 1;
+        //check for column
+        while (counter_y < 9)
+        {
+            if (check_position >= 73 && check_position <= 81)//check if last row was reached
+            {
+                while (check_position > 9)
+                {
+                    check_position -= 9;
+                }
+            }
+            if (Board[place] == Board[check_position] && place != check_position)
+            {
+                Console.WriteLine("BROKE IN Column");
+                return true;
+            }
+            check_position += 9;
+            counter_y++;
+        }
+        return false;
+    }
+    private bool check_box(int check_position, int max_place_box, int place, int Num_above_it, int[] Board)
+    {
+        bool error_box = false;
+        int column = place;//will be used to set it to first row first column of box
+        //for 3x3 box
+        check_position = place - 9;
+        while (check_position > 0)
+        {
+            check_position -= 9;
+            Num_above_it++;
+        }
+        check_position += 9;
+
+        if (Num_above_it % 3 == 0)//first row of box
+        {
+            if (check_position % 3 == 0)//third element in row
+            {
+                column -= 2;
+            }
+            else if (check_position % 3 == 2) //second element in row
+            {
+                column -= 1;
+            }
+        }
+
+        else if ((Num_above_it % 3) == 1) // second row
+        {
+            if (check_position % 3 == 0)//third element in row
+            {
+                column -= 11;
+            }
+            else if (check_position % 3 == 2) //second element in row
+            {
+                column -= 10;
+            }
+            else //first element in row
+            {
+                column -= 9;
+            }
+        }
+        else
+        {
+            if (check_position % 3 == 0)//third element in row
+            {
+                column -= 20;
+            }
+            else if (check_position % 3 == 2) //second element in row
+            {
+                column -= 19;
+            }
+            else
+            {
+                column -= 18;
+            }
+        }
+        for (int i = column; i < column + 3; i++)
+        {
+            if (Board[i] == Board[place] && i != place)
+            {
+                Console.WriteLine("Broke in Box");
+                error_box = true;
+            }
+        }
+        for (int i = column; i < column + 9 * 3; i += 9)
+        {
+            if (Board[i] == Board[place] && i != place)
+            {
+                Console.WriteLine("Broke in Box");
+                error_box = true;
+            }
+        }
+        for (int i = column + 1; i < column + 9 * 3; i += 9)
+        {
+            if (Board[i] == Board[place] && i != place)
+            {
+                Console.WriteLine("Broke in Box");
+                error_box = true;
+            }
+        }
+        for (int i = column + 2; i < column + 9 * 3; i += 9)
+        {
+            if (Board[i] == Board[place] && i != place)
+            {
+                Console.WriteLine("Broke in Box");
+                error_box = true;
+            }
+        }
+        return error_box;
+    }
+}
